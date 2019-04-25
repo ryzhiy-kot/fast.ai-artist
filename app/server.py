4a352af52f18a80ccc1388396b525b26937280b0
ryzhiy kot <kyatsenko@gmail.com>
2019-04-26 00:17:37 +0800

    one more dir fix

--- app/server.py
+++ app/server.py
@@ -27,7 +27,7 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}')
     try:
-        learn = load_learner(path, path/'models'/f'{model_file_name}')
+        learn = load_learner(path/'models', f'{model_file_name}')
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
